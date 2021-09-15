a34eb461d90d6c1421087b277b80dbc5868a9d06
Your Name <olsson.perry@gmail.com>
2021-09-15 12:01:21 -0700

    operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -40,7 +40,53 @@ public class Main {
 
         // if-then
         boolean isAlien = false;
-        if (isAlien == false)
+        if (isAlien == false) {
             System.out.println("It is not an alien!");
+            System.out.println("And I'm scared of aliens");
+        }
+
+        // Logical AND Operator
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        // Logical OR operator
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        // Assignment Operator VS Equals to Operator
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("is car is false");
+        }
+
+        // ternary operator
+
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("isCar was a car");
+        }
+
+        //challenge
+
+        double myDouble = 20.00d;
+        double mySecondDouble = 80.00d;
+        boolean isZero = ((myDouble + mySecondDouble) * 100d) % 40 == 0 ? true : false;
+        System.out.println("isZero = " + isZero);
+        if (!isZero) {
+            System.out.println("Got some remainder");
+        }
     }
 }
